b122b067aabdf580070d4e3e9ab064b5b81c49ea
lancebrudnicki <lancebrudnicki@gmail.com>
2021-07-16 15:59:30 -0600

    commit

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,38 @@
+
+const Product = require('../models/product.model');
+
+
+module.exports = {
+    createProduct: (req,res) =>{
+        Product.create(req.body)
+        .then(product => res.json({result:product}))
+        .catch(err => res.json(err.errors))
+    },
+    showOne: (req, res) => {
+        product.findOne({_id: req.params.id})
+        .then(data => res.json({ results: data }))
+        .catch(err => res.json(err.errors))
+    }
+}
+
+
+// module.exports.createProduct =(request, response) => {
+//     console.log(request)
+//     // console.log(request.body)
+//     Product.create(request.body)
+//         .then(product => response.json(product))
+//         .catch(err => response.json(err))
+// }
+
+// module.exports.findOne =(req, res) => {
+//     Product.findOne({_id: req.params.id})
+//     .then(product => res.json(product))
+//     .catch(err => res.json(err.errors))
+// }
+
+module.exports.index =(req, response) => {
+    response.json({
+        message: "hello world"
+    });
+}
+
